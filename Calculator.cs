99847c0faf63c8a754e6ab8611c172d0b85dfe46
Kiki <codingki@gmail.com>
2020-03-10 12:12:15 +0700

    perubahan method main

--- Calculator.cs
+++ Calculator.cs
@@ -1,6 +1,10 @@
 class Calculator{
     static void Main(string[] args){
-        
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a,b, Penambahan(a,b));
+        Console.WriteLine("Hasil pengurangan: {0} + {1} = {2}", a,b, Pengurangan(a,b));
     }
     static int Penambahan(int a, int b){
         return a + b;
